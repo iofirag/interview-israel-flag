61801b46ca0e1dfb3420986fc0275e111e1d83cc
iofirag <iofirag@gmail.com>
2024-05-12 11:31:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import random
 # height = int(input('Enter hight: '))
 width=60
 height=30
+specialChar = '*'
+emptyChar = ' '
 # ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
 # ________________
 def isLines(h, w, height, width):
@@ -13,24 +15,23 @@ def isLines(h, w, height, width):
     
 # ✡
 def isMagenDavid(h, w, height, width):
-    # △
-    triangleUp = (
-        h>=0.23*height and h<0.65*height and w>=0.3*width and w<0.7*width and (
+    triangleUp = False
+    triangleDown = False
+    if 0.3*width <= w <= 0.7*width:
+        # △
+        triangleUp = 0.23*height <= h < 0.65*height and (
             (h>=0.62*height) or # _
             -round(0.38*width) in (h-(width-w), h-w) # /\
         )
-    )
-    # ▽
-    triangleDown = (
-        h>=0.33*height and h<0.75*height and w>=0.3*width and w<0.7*width and (
+        # ▽
+        triangleDown = 0.33*height <= h < 0.75*height and (
             (h<0.36*height) or # ‾
             -round(0.13*width) in (h-(width-w), h-w) # \/
-        ) 
-    )
+        )
     return triangleUp or triangleDown
     
 for h in range(height):
     for w in range(width):
         bit = isLines(h,w,height,width) or isMagenDavid(h,w,height,width)
-        print('*' if bit else '0', end =" ")
+        print(specialChar if bit else emptyChar, end =" ")
     print()
